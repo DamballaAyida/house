411bf0f83db022973b6703012c70082e12a45477
PJ Rivera <pj.rivera.perez@gmail.com>
2019-03-19 21:11:03 +0100

    Cuarto comit

--- Triangle.java
+++ Triangle.java
@@ -1,9 +1,9 @@
 import java.awt.*;
-
+//Prueba de cambio para git
 /**
  * A triangle that can be manipulated and that draws itself on a canvas.
  * 
- * @author  Michael Kšlling and David J. Barnes
+ * @author  Michael Kï¿½lling and David J. Barnes
  * @version 2016.02.29
  */
 
